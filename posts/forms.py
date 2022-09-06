b736d69199c8f9d35ae3b61a265d80efc6f9c800
uLNsBV <ulan.eshbaev571@gmail.com>
2022-09-06 09:05:30 +0600

    bug fix on post.views&index.html

--- posts/forms.py
+++ posts/forms.py
@@ -2,8 +2,8 @@ from pdb import post_mortem
 from django import forms
 from posts.models import Post
 
+
 class CreatePostForm(forms.ModelForm):
-  class Meta:
-    model = Post
-    fields = '__all__'
-    
+    class Meta:
+        model = Post
+        fields = ('name', 'description', 'image')
--- posts/views.py
+++ posts/views.py
@@ -11,15 +11,15 @@ def index_page(request):
     title = request.GET.get('title')
     posts = Post.objects.all()
     if title:
-        posts = Post.objects.filter(Q(name__icontains=title)) | Q(
-            description__icontains=title)
+        posts = Post.objects.filter(Q(name__icontains=title) | Q(
+            description__icontains=title))
     return render(request, 'index.html', locals())
 
 
 class CreatePostView(generic.CreateView):
     template_name = 'create_post.html'
     model = Post
-    form_class = CreatePostForm
+    form_class = CreatePostForm #to create that very form
 
     def get_success_url(self) -> str:
         return reverse('index')
